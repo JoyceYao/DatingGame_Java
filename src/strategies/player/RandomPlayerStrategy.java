d86dde698a3b2bee1e519759623ef16b107e053f
JoyceYao <watasi0839@gmail.com>
2015-11-20 23:28:11 -0500

    add modify logic

--- src/strategies/player/RandomPlayerStrategy.java
+++ src/strategies/player/RandomPlayerStrategy.java
@@ -2,22 +2,30 @@ package strategies.player;
 
 import game.StrategyService;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomPlayerStrategy extends PlayerStrategy {
 	
 	Random r = new Random();
+	float[] origin;
 
 	@Override
 	public float[] getInitialWeights() {
-		return getRandomWeight();
+		origin = getRandomWeight();
+		return origin;
 	}
 
 	@Override
 	public float[] getNextWeights() {
-		return getRandomWeight();
+		//return getRandomWeight();
+		return origin;
 	}
 	
+	/* randomly create positive and negative values
+	 * verify: positive number sum to 1, negative number sum to 0
+	 * then shuffle it 
+	 */
 	private float[] getRandomWeight(){
 		float[] result = new float[attributeNo];
 		float positiveNo = r.nextInt((attributeNo-1)/2);
@@ -68,6 +76,27 @@ public class RandomPlayerStrategy extends PlayerStrategy {
 		printArray(result);
 		return result;
 	}
+	
+	/* 
+	 * 
+	 * /
+	private float[] RandomModifyWeight(){
+		int modifyNo = (int)Math.floor((double)attributeNo*0.05);
+		float[] result = Arrays.copyOf(origin, origin.length);
+		while(modifyNo > 0){
+			int idx = r.nextInt(attributeNo);
+			if(result[idx] == origin[idx] && result[idx] != 0){
+				float modifyRate = r.nextFloat()/5;
+				boolean isAdd = r.nextFloat() > 0.5? true : false;
+				if(isAdd){
+					result[idx] += result[idx]*modifyRate;
+				}else{
+					result[idx] -= result[idx]*modifyRate;
+				}
+			}
+			
+		}
+	}*/
 
 	private void shuffleArray(float[] ar){
 		for (int i = ar.length - 1; i > 0; i--){
